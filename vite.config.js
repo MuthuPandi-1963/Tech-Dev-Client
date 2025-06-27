0aca22a7975bef9e1b048b29724e9a266067c5a3
MuthuPandi-1963 <muthupandir74738@gmail.com>
2025-06-27 10:01:22 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"./",
   define: {
     "process.env": {
       VITE_BACKEND_URL_GLOBAL: process.env.VITE_BACKEND_URL_GLOBAL,
