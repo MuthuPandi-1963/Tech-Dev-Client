b2345bb69fdaca02d0eea701fb2fd3ed604607b8
MuthuPandi-1963 <muthupandir74738@gmail.com>
2025-06-30 11:18:20 +0530

    change router config

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import { HashRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import './index.css'
 import App from './App.jsx'
@@ -9,13 +9,12 @@ import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById('root')).render(
+  <HashRouter>
   <Provider store={Store}>
   <AuthContextProvider>
-  <BrowserRouter>
     <App/>
-    <ToastContainer
-/>
-  </BrowserRouter>
+    <ToastContainer/>
   </AuthContextProvider>
   </Provider>
+  </HashRouter>
 )
